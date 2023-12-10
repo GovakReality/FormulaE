a9685d33715359d0a978c74ab2c12d646def8768
jlhaas-govak <123078412+jlhaas-govak@users.noreply.github.com>
2023-12-10 19:26:06 -0300

    app bar implementation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import ThreeScene from './components/ThreeScene.vue'
-import InitialScreen from './components/InitialScreen.vue';
+import TopBar from './components/TopBar.vue';
 import PositionButton from './components/PositionButton.vue';
 import LoadingScreen from './components/LoadingScreen.vue';
 </script>
 
 <template>
   <v-app>
-    <v-app-bar flat color="transparent"></v-app-bar>
-    <v-main>
+    <TopBar />
+    <v-main class="overflow-hidden">
       <PositionButton />
       <ThreeScene />
       <LoadingScreen />
--- src/main.js
+++ src/main.js
@@ -8,12 +8,20 @@ import './style.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const pinia = createPinia();
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },  
 });
 
 createApp(App).use(pinia).use(vuetify).mount('#app')

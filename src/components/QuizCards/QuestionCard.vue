054a93b64cb7c34655ba361e4a60167de0b86451
Guilherme Novak <guilherme@govakreality.com>
2024-01-13 22:35:15 -0300

    Tweaks to HUD and First Three Cards

--- src/components/QuizCards/QuestionCard.vue
+++ src/components/QuizCards/QuestionCard.vue
@@ -1,275 +1,275 @@
 <script setup>
-  import { useCardsStore } from '/src/stores/CardsStore';
-  import { useQuizStore } from '/src/stores/QuizStore';
-  import { ref, watch, computed } from 'vue';
-  import { storeToRefs } from 'pinia';
-  import { useLocale } from 'vuetify';
+import { useCardsStore } from '/src/stores/CardsStore';
+import { useQuizStore } from '/src/stores/QuizStore';
+import { ref, watch, computed } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useLocale } from 'vuetify';
 
-  const { isRtl, current } = useLocale();
+const { isRtl, current } = useLocale();
 
-  const cardsStore = useCardsStore();
-  const { cardIndex } = storeToRefs(cardsStore);
-  const quizStore = useQuizStore();
-  const { question, round, scoreFixed, shouldCameraMove, quizEnded } = storeToRefs(quizStore);
+const cardsStore = useCardsStore();
+const { cardIndex } = storeToRefs(cardsStore);
+const quizStore = useQuizStore();
+const { question, round, scoreFixed, shouldCameraMove, quizEnded } = storeToRefs(quizStore);
 
-  const expand = ref(false);
-  const show = ref(false);
-  const expandHud = ref(false);
-  const genType = ref('');
+const expand = ref(false);
+const show = ref(false);
+const expandHud = ref(false);
+const genType = ref('');
 
-  const maxPoints = parseFloat(10000); 
-  const timeLeft = ref(maxPoints);
-  let animFrame;
-  let prevTime;
-  let clockStep = 10;
+const maxPoints = parseFloat(10000);
+const timeLeft = ref(maxPoints);
+let animFrame;
+let prevTime;
+let clockStep = 10;
 
-  watch(cardIndex, () => {
-    if (cardIndex.value >= 2 && cardIndex.value < 11) {
-      switch (cardIndex.value) {
-        case (2):
-        case (3):
-        case (4):
-          genType.value = 'gen3';
-          break;
-        case (5):
-        case (6):
-        case (7):
-          genType.value = 'gen2';
-          break;
-        case (8):
-        case (9):
-        case (10):
-          genType.value = 'gen1';
-          break;
-        default:
-          genType.value = 'gen3';
-      };
-      show.value = true; 
-      expandCard();
-    } else {
-      show.value = false;
-    }      
-  });
+watch(cardIndex, () => {
+  if (cardIndex.value >= 2 && cardIndex.value < 11) {
+    switch (cardIndex.value) {
+      case (2):
+      case (3):
+      case (4):
+        genType.value = 'gen3';
+        break;
+      case (5):
+      case (6):
+      case (7):
+        genType.value = 'gen2';
+        break;
+      case (8):
+      case (9):
+      case (10):
+        genType.value = 'gen1';
+        break;
+      default:
+        genType.value = 'gen3';
+    };
+    show.value = true;
+    expandCard();
+  } else {
+    show.value = false;
+  }
+});
 
-  const expandCard = () => {
-    quizStore.newQuestion(genType.value);
-    quizStore.incrementRound();
-    shouldCameraMove.value = true;
-    setTimeout(() => {
-      expand.value = true;
-      if(cardIndex.value == 2) {
-        expandHud.value = true;
-      }
-    }, 100);
-  };
-
-  const contractCard = () => {
-    expand.value = false;
-    if(cardIndex.value == 10) {
-        expandHud.value = false;
-        quizEnded.value = true;
-        shouldCameraMove.value = true;
-      }    
-  };
-
-  const onClick = (val, event) => {
-    cancelAnimationFrame(animFrame);
-    if (val == question.value.correct) {
-      quizStore.addScore(timeLeft.value);
-      showCorrectAnswer();
-    } else {
-      showCorrectAnswer();
-      showWrongAnswer(event);
+const expandCard = () => {
+  quizStore.newQuestion(genType.value);
+  quizStore.incrementRound();
+  shouldCameraMove.value = true;
+  setTimeout(() => {
+    expand.value = true;
+    if (cardIndex.value == 2) {
+      expandHud.value = true;
     }
-    setTimeout(() => {
+  }, 100);
+};
+
+const contractCard = () => {
+  expand.value = false;
+  if (cardIndex.value == 10) {
+    expandHud.value = false;
+    quizEnded.value = true;
+    shouldCameraMove.value = true;
+  }
+};
+
+const onClick = (val, event) => {
+  cancelAnimationFrame(animFrame);
+  if (val == question.value.correct) {
+    quizStore.addScore(timeLeft.value);
+    showCorrectAnswer();
+  } else {
+    showCorrectAnswer();
+    showWrongAnswer(event);
+  }
+  setTimeout(() => {
+    contractCard();
+  }, 300);
+};
+
+const showCorrectAnswer = () => {
+  //correct answer
+}
+
+const showWrongAnswer = (ev) => {
+  //wrong answer
+}
+
+const onAfterLeave = (el) => {
+  cardsStore.incrementCardIndex();
+}
+
+const onAfterEnter = (el) => {
+  startTimer();
+}
+
+const startTimer = () => {
+  timeLeft.value = maxPoints;
+  prevTime = performance.now();
+  timer(); // COMMENT THIS TO STOP
+};
+
+const timer = () => {
+  let aux = performance.now();
+  let calc = (aux - prevTime);
+  if (calc > clockStep) {
+    timeLeft.value = (timeLeft.value - calc).toFixed(0);
+    if (timeLeft.value <= 0) {
+      timeLeft.value = 0;
+      cancelAnimationFrame(animFrame);
       contractCard();
-    }, 300);    
-  };
-  
-  const showCorrectAnswer = () => {
-    //correct answer
-  }  
-  
-  const showWrongAnswer = (ev) => {
-    //wrong answer
-  }  
-
-  const onAfterLeave = (el) => {
-    cardsStore.incrementCardIndex();
-  }  
-   
-  const onAfterEnter = (el) => {
-    startTimer();
-  }  
-
-  const startTimer = () => {
-    timeLeft.value = maxPoints;
-    prevTime = performance.now();
-    timer();
-  };
-
-  const timer = () => {
-    let aux = performance.now();
-    let calc = (aux - prevTime);
-    if (calc > clockStep) {
-      timeLeft.value = (timeLeft.value - calc).toFixed(0);
-      if(timeLeft.value <= 0) {
-        timeLeft.value = 0;
-        cancelAnimationFrame(animFrame);
-        contractCard();
-      } else {
-        prevTime = aux;
-        animFrame = requestAnimationFrame(timer);
-      }
     } else {
+      prevTime = aux;
       animFrame = requestAnimationFrame(timer);
     }
-  };
-
-  const timeLeftFixed = computed(() => {
-    return (timeLeft.value / 1000).toFixed(3).replace(".",",");
-  });
-
-  const timeBar = computed(() => {
-    let x = normalizeToRange(timeLeft.value, 0, maxPoints, 0, 100);
-    return x.toFixed(0);
-  });
- 
-  const roundWithPrecision = (num, precision) => {
-    var multiplier = Math.pow(10, precision);
-    return Math.round( num * multiplier ) / multiplier;
+  } else {
+    animFrame = requestAnimationFrame(timer);
   }
-  const normalizeToRange = (value, oldMin, oldMax, newMin, newMax) => (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+};
+
+const timeLeftFixed = computed(() => {
+  return (timeLeft.value / 1000).toFixed(3).replace(".", ",");
+});
+
+const timeBar = computed(() => {
+  let x = normalizeToRange(timeLeft.value, 0, maxPoints, 0, 100);
+  return x.toFixed(0);
+});
+
+const roundWithPrecision = (num, precision) => {
+  var multiplier = Math.pow(10, precision);
+  return Math.round(num * multiplier) / multiplier;
+}
+const normalizeToRange = (value, oldMin, oldMax, newMin, newMax) => (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
 </script>
 
 <template>
-  <v-sheet v-if="show" class="d-flex flex-column flex-sm-row align-center align-sm-end justify-center h-100 pa-2 pa-sm-10 pb-sm-16 pb-sm-16">
-    <v-slide-y-reverse-transition
-    @after-leave="onAfterLeave"
-    @after-enter="onAfterEnter"
-    group
-    >
-      <v-card
-      v-if="expand"
-      class="g-card py-5 px-4 rounded-xl"
-      color="#F0F0F0"
-      variant="flat"
-      >
-      <template v-slot:loader="{ isActive }">
-        <v-progress-linear
-          :active="true"
-          :model-value="timeBar"
-          color="#28673c"
-          bg-color="#28673c"
-          height="5"
-        ></v-progress-linear>
-      </template>
+  <v-sheet v-if="show"
+    class="d-flex flex-column flex-sm-row align-center align-sm-end justify-center h-100 pa-2 pa-sm-10 pb-sm-16 pb-sm-16">
+    <v-slide-y-reverse-transition @after-leave="onAfterLeave" @after-enter="onAfterEnter" group>
+      <v-card v-if="expand" class="g-card py-4 px-4 rounded-xl" color="#F0F0F0" variant="flat">
+        <template v-slot:loader="{ isActive }">
+          <v-progress-linear :active="true" :model-value="timeBar" color="#28673c" bg-color="#28673c"
+            height="5"></v-progress-linear>
+        </template>
 
         <v-card-item>
-          <div class="g-round mb-0 mb-sm-1 font-weight-bold" 
-          :class="{ 'g-round-l-def': !isRtl, 'g-round-l-rtl': isRtl }"
-          >
+          <div class="g-round mb-0 mb-sm-1 font-weight-bold" :class="{ 'g-round-l-def': !isRtl, 'g-round-l-rtl': isRtl }">
             {{ $t("global.round") }} {{ round }}
           </div>
           <div class="g-text">
             {{ question[current].question }}
-          </div>                     
+          </div>
         </v-card-item>
         <v-card-actions class="text-center justify-center px-3">
 
           <v-container fluid class="pa-0">
             <v-row dense>
-              <v-col class="pa-2"> 
-                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold" @click="onClick(1, $event)">
+              <v-col class="pa-2">
+                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold"
+                  @click="onClick(1, $event)">
                   {{ question[current].answer1 }}
                 </v-btn>
               </v-col>
               <v-col class="pa-2">
-                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold" @click="onClick(2, $event)">
+                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold"
+                  @click="onClick(2, $event)">
                   {{ question[current].answer2 }}
                 </v-btn>
               </v-col>
             </v-row>
             <v-row dense>
               <v-col class="pa-2">
-                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold" @click="onClick(3, $event)">
+                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold"
+                  @click="onClick(3, $event)">
                   {{ question[current].answer3 }}
-                </v-btn> 
+                </v-btn>
               </v-col>
               <v-col class="pa-2">
-                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold" @click="onClick(4, $event)">
+                <v-btn block rounded="xl" :slim="false" color="#F0F0F0" class="g-bt font-weight-bold"
+                  @click="onClick(4, $event)">
                   {{ question[current].answer4 }}
-                </v-btn> 
-              </v-col>              
+                </v-btn>
+              </v-col>
             </v-row>
-          </v-container>             
+          </v-container>
         </v-card-actions>
       </v-card>
     </v-slide-y-reverse-transition>
-    <v-sheet v-if="show" class="g-hud"
-    :class="{ 'g-hud-l-def': !isRtl, 'g-hud-l-rtl': isRtl }"
-    >
+    <v-sheet v-if="show" class="g-hud" :class="{ 'g-hud-l-def': !isRtl, 'g-hud-l-rtl': isRtl }">
       <v-slide-y-reverse-transition group>
         <v-sheet v-if="expandHud" class="g-hud-w">
-          <div class="g-hud-total px-5 py-2">{{scoreFixed}} {{ $t("global.pts") }}</div>
-          <div class="g-hud-round px-5 py-2">{{ $t("global.round") }} 
+          <div v-if="!isRtl" class="g-hud-total g-hud-total-def px-4 py-1">{{
+            scoreFixed }} {{ $t("global.pts") }}</div>
+          <div v-if="isRtl" class="g-hud-total g-hud-total-rtl px-4 py-1">{{
+            scoreFixed }} {{ $t("global.pts") }}</div>
+          <div class="g-hud-round px-5 py-1">{{ $t("global.round") }}
             <span v-if="!isRtl">0{{ round }}/09</span>
             <span v-if="isRtl">09/0{{ round }}</span>
           </div>
-          <div class="g-hud-score px-5 py-2">+{{timeLeftFixed}} {{ $t("global.pts") }}</div>
-        </v-sheet>    
-      </v-slide-y-reverse-transition>        
+          <div class="g-hud-score pr-4 pl-1 py-1">+{{ timeLeftFixed }} {{ $t("global.pts") }}</div>
+        </v-sheet>
+      </v-slide-y-reverse-transition>
     </v-sheet>
   </v-sheet>
-  
 </template>
 
 <style scoped>
-.g-card{
+.g-card {
   max-width: 100%;
-  width: 594px;
+  width: 550px;
   color: #28673c;
-  margin-bottom: 90px;
+  margin-bottom: 7dvh;
+  /* margin-bottom: 64px */
 }
+
 .g-round {
-  font-size: clamp(22px, 3dvh, 24px);
+  font-size: clamp(18px, 3dvh, 20px);
   color: #28673c;
   line-height: normal;
 }
+
 .g-round-l-def {
   text-align: left;
 }
+
 .g-round-l-rtl {
   text-align: right;
 }
+
 .g-text {
   color: #28673c;
   font-weight: 400;
   font-size: clamp(20px, 3dvh, 24px);
   line-height: clamp(30px, 3.5dvh, 32px);
-  padding-top: 8px;
-  padding-bottom: 8px;
+  padding-top: 1px;
+  padding-bottom: 0px;
 }
+
 .g-bt {
-  font-size: clamp(16px, 2.3dvh, 18px);
-  width: 254px;
+  font-size: clamp(12px, 2.3dvh, 15px);
+  width: 230px;
   max-width: 100%;
-  line-height: normal;
+  line-height: 18px;
   background: linear-gradient(94deg, #28673C 7.42%, #07361C 166.68%);
   text-wrap: balance;
 }
-.g-correct { 
+
+.g-correct {
   border: 3px solid #261bc0;
 }
-.g-wrong { 
+
+.g-wrong {
   border: 3px solid #ff0000;
 }
+
 :deep(.v-btn.v-btn--density-default) {
-  height: clamp(60px, 6dvh, 70px);
+  height: clamp(45px, 6dvh, 55px);
 }
+
 :deep(.v-btn__content) {
   white-space: normal;
 }
+
 .g-hud {
   font-family: IBM Plex Sans;
   background-color: transparent;
@@ -278,131 +278,170 @@
   max-width: 100%;
   bottom: 45px;
 }
+
 .g-hud-l-def {
   right: 38px;
 }
+
 .g-hud-l-rtl {
   left: 38px;
 }
+
 .g-hud-w {
   background-color: transparent;
 }
+
 .g-hud-round {
   opacity: 0.9;
   background-color: #F3F5F4;
   font-weight: 700;
-  font-size: 17px;
-  width: 150px;
-  height: 40px;
-  color: #000000;  
+  font-size: 16px;
+  width: 140px;
+  height: 30px;
+  color: #000000;
   display: inline-block;
   vertical-align: middle;
 }
+
 .g-hud-score {
   background-color: #28673C;
   font-weight: 700;
-  font-size: 18px;
-  color: #F0F0F0;  
+  font-size: 16px;
+  color: #F0F0F0;
   text-align: right;
-  width: 150px;
-  height: 40px;
+  width: 130px;
+  height: 30px;
   display: inline-block;
   vertical-align: middle;
 }
+
 .g-hud-total {
-  background-color: #28673C;
+  background-color: #28673C00;
   font-weight: 700;
-  font-size: 18px;
-  color: #F0F0F0;  
+  font-size: 16px;
+  color: #F0F0F0;
   text-align: right;
   vertical-align: bottom;
-  width: 300px;
   max-width: 100%;
+  text-shadow:
+    1px 1px 0 #28673C,
+    -1px 1px 0 #28673C,
+    -1px -1px 0 #28673C,
+    1px -1px 0 #28673C;
+}
+
+.g-hud-total-def {
+  width: 130px;
+  margin-left: 140px;
+}
+
+.g-hud-total-rtl {
+  width: 130px;
+  margin-right: 140px;
 }
 
 :deep(.v-card__loader) {
   bottom: 0;
   top: auto;
 }
+
 :deep(.v-progress-linear__background) {
   opacity: 0.3;
 }
+
 :deep(.v-btn__content) {
   padding-top: 2px;
 }
+
 @media (max-width: 599px) {
-  .g-card{
+  .g-card {
     width: 420px;
     margin-bottom: 20px;
   }
+
   .g-round {
     font-size: 18px;
   }
+
   .g-text {
     font-size: 19px;
-  } 
+  }
+
   .g-bt {
     font-size: 15px;
     width: 184px;
-  } 
+  }
+
   :deep(.v-btn.v-btn--density-default) {
     height: 50px;
-  } 
+  }
+
   .g-hud {
     position: relative;
     bottom: auto;
     right: auto;
     left: auto;
-  } 
+  }
+
   .g-hud-round {
     width: 150px;
   }
+
   .g-hud-score {
     width: 150px;
-  }        
+  }
 }
 
 @media (max-width: 446px) {
   .g-hud-round {
-    font-size: 16px; 
-  }  
+    font-size: 16px;
+  }
+
   .g-hud-score {
-    font-size: 16px; 
-  }      
+    font-size: 16px;
+  }
+
   .g-hud-total {
-    font-size: 16px; 
-  }   
+    font-size: 16px;
+  }
 }
 
 @media (min-width: 2560px) {
-  .g-card{
+  .g-card {
     width: 710px;
   }
+
   .g-round {
     font-size: 30px;
   }
+
   .g-text {
     font-size: 32px;
-  } 
+  }
+
   .g-bt {
     font-size: 25px;
-  } 
+  }
+
   :deep(.v-btn.v-btn--density-default) {
     height: 82px;
-  } 
+  }
+
   .g-hud-round {
     font-size: 24px;
     height: 52px;
     width: 200px;
   }
+
   .g-hud-score {
     font-size: 26px;
     width: 200px;
     height: 52px;
   }
+
   .g-hud-total {
     font-size: 26px;
     width: 400px;
-  }    
+  }
 }
 </style>
--- src/components/QuizCards/WelcomeCard.vue
+++ src/components/QuizCards/WelcomeCard.vue
@@ -1,67 +1,59 @@
 <script setup>
-  import { useCardsStore } from '/src/stores/CardsStore';
-  import { useLoadingStore } from '/src/stores/LoadingStore';
-  import { ref, watch, onMounted} from 'vue';
-  import { storeToRefs } from 'pinia';
-  import saudiaLogo from '/images/SaudiaLogo.png';
-  
-  const cardsStore = useCardsStore();
-  const { cardIndex } = storeToRefs(cardsStore);
-  const loadingStore = useLoadingStore();
-  const { loadStart, loadComplete, loadError, loadProgress } = storeToRefs(loadingStore);
-  const expand = ref(false);
-  const show = ref(false);
+import { useCardsStore } from '/src/stores/CardsStore';
+import { useLoadingStore } from '/src/stores/LoadingStore';
+import { ref, watch, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import saudiaLogo from '/images/SaudiaLogo.png';
 
-  
-  watch(loadComplete, (val) => {
-    if (loadComplete.value) {
-      expand.value = true;
-    }
-  });
+const cardsStore = useCardsStore();
+const { cardIndex } = storeToRefs(cardsStore);
+const loadingStore = useLoadingStore();
+const { loadStart, loadComplete, loadError, loadProgress } = storeToRefs(loadingStore);
+const expand = ref(false);
+const show = ref(false);
 
-  watch(cardIndex, () => {
-    if (cardIndex.value == 0) {
-      show.value = true;
-      setTimeout(() => expand.value = true, 100);
-    } else {
-      expand.value = false;
-    }
-  });
 
-  const onClick = (event) => {
-    expand.value = false;
-  };
+watch(loadComplete, (val) => {
+  if (loadComplete.value) {
+    expand.value = true;
+  }
+});
 
-  const onAfterLeave = (el) => {
-    show.value = false;
-    cardsStore.incrementCardIndex();
-  }  
-
-  onMounted(() => {
+watch(cardIndex, () => {
+  if (cardIndex.value == 0) {
     show.value = true;
-  });  
- 
+    setTimeout(() => expand.value = true, 100);
+  } else {
+    expand.value = false;
+  }
+});
+
+const onClick = (event) => {
+  expand.value = false;
+};
+
+const onAfterLeave = (el) => {
+  show.value = false;
+  cardsStore.incrementCardIndex();
+}
+
+onMounted(() => {
+  show.value = true;
+});
+
 </script>
 
 <template>
-  <v-sheet v-if="show" class="d-flex align-center align-sm-end justify-center justify-sm-end h-100 px-2 px-sm-10 pb-sm-10">
+  <v-sheet v-if="show" class="d-flex align-center align-sm-end justify-center justify-sm-end h-100 px-2 px-sm-9 pb-sm-10">
     <v-slide-y-reverse-transition @after-leave="onAfterLeave">
-      <v-card
-      v-if="expand"
-      class="g-card py-5 rounded-xl"
-      variant="flat"
-      >
+      <v-card v-if="expand" class="g-card py-4 rounded-xl" variant="flat">
         <v-card-item class="text-center">
-        
-          <v-img
-            :src=saudiaLogo
-            width="138"
-            class="text-center justify-center mx-auto"
-          ></v-img>
+
+          <v-img :src=saudiaLogo width="128" class="text-center justify-center mx-auto"></v-img>
           <h3 class="g-title">
             {{ $t("welcome.title") }}
-          </h3>          
-          
+          </h3>
+
           <h3 class="g-text">
             {{ $t("welcome.subtitle") }}
           </h3>
@@ -77,35 +69,37 @@
 </template>
 
 <style scoped>
-.g-card{
+.g-card {
   background: linear-gradient(69deg, #07361C 9.61%, #07361C 9.61%, #026536 80.85%);
   max-width: 100%;
-  width: 413px;
+  width: 285px;
   color: #F0F0F0;
 }
 
 .g-title {
   font-weight: bold;
-  font-size: clamp(18px, 3.6dvh, 28px);
-  line-height: clamp(30px, 4.2dvh, 36px);
+  font-size: clamp(18px, 3.6dvh, 22px);
+  line-height: clamp(28px, 4.2dvh, 29px);
   padding-top: clamp(12px, 3.4dvh, 32px);
-  padding-bottom: clamp(12px, 3dvh, 30px);
+  padding-bottom: clamp(12px, 3dvh, 20px);
   padding-left: 14px;
   padding-right: 14px;
 }
+
 .g-text {
   font-weight: 400;
-  font-size: clamp(18px, 3dvh, 26px);
-  line-height: clamp(30px, 3.5dvh, 34px);
+  font-size: clamp(16px, 3dvh, 20px);
+  line-height: clamp(27px, 3.5dvh, 28px);
   padding-bottom: 4px;
   padding-left: 28px;
   padding-right: 28px;
 }
+
 .g-bt-start {
   font-size: clamp(16px, 2.3dvh, 18px);
   width: 183px;
   max-width: 100%;
-  line-height: normal;  
+  line-height: normal;
 }
 
 :deep(.v-btn--variant-tonal .v-btn__underlay) {
@@ -113,7 +107,7 @@
 }
 
 :deep(.v-btn.v-btn--density-default) {
-  height: clamp(32px, 5.4dvh, 46px);
+  height: clamp(32px, 6.4dvh, 44px);
 }
 
 :deep(.v-btn__content) {
@@ -123,23 +117,26 @@
 @media (max-width: 599px) {
   .g-card {
     width: 333px;
-  } 
+  }
 }
 
 @media (min-width: 2560px) {
-  .g-card{
+  .g-card {
     width: 513px;
   }
+
   .g-title {
     font-size: 32px;
     line-height: 38px;
-  }  
+  }
+
   .g-bt-start {
     font-size: 22px;
     width: 220px;
-  } 
+  }
+
   :deep(.v-btn.v-btn--density-default) {
     height: 56px;
-  }  
+  }
 }
 </style>

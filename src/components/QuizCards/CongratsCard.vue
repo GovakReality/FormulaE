59dd98c8db58c2dc3198bb667ac4b75f2473b503
Guilherme Novak <guilherme@govakreality.com>
2024-01-14 00:54:18 -0300

    Better Congrats Card

--- src/components/QuizCards/CongratsCard.vue
+++ src/components/QuizCards/CongratsCard.vue
@@ -107,7 +107,7 @@ const onAfterLeave = (el) => {
 </script>
 
 <template>
-  <v-sheet v-if="show" class="d-flex flex-column align-center justify-center h-100">
+  <v-sheet v-if="show" class="d-flex flex-column align-center justify-center h-95">
     <v-slide-y-reverse-transition @after-leave="onAfterLeave" group>
       <v-sheet v-if="expand" class="g-sheet" position="relative" color="transparent">
         <v-card class="g-card py-4 rounded-xl" variant="flat">
@@ -313,6 +313,11 @@ const onAfterLeave = (el) => {
 
 :deep(.v-btn__content) {
   padding-top: 2px;
+}
+
+:deep(.v-input--density-default .v-field--variant-solo .v-label.v-field-label--floating) {
+  top: 3px;
+  font-size: 10px;
 }
 
 @media (max-width: 599px) {

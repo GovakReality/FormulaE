ae60073a1325f94a6c903e9e8d501340aa9131c1
jlhaas-govak <123078412+jlhaas-govak@users.noreply.github.com>
2024-01-18 14:12:36 -0300

    prevent zoom on IOS test

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { onMounted } from 'vue';
 import ThreeScene from './components/ThreeScene.vue'
 import TopBar from './components/TopBar/TopBar.vue';
 import QuizCards from './components/QuizCards/QuizCards.vue';
@@ -7,6 +8,22 @@ import LoadingScreen from './components/LoadingScreen.vue';
 import GenInfo from './components/GenInfo.vue';
 /* import LiveInspector from './components/LiveInspector.vue'; */
 import InactivityAlert from './components/InactivityAlert.vue';
+
+onMounted(() => {
+  //disable zoom on ios
+  document.addEventListener('touchmove', function (event) {
+    if (event.scale !== 1) { event.preventDefault(); }
+  }, false)
+  //disable double tap zoom on ios
+  var lastTouchEnd = 0;
+  document.addEventListener('touchend', function (event) {
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+  });
 </script>
 
 <template>

f38726af6ae7fd299c9794b2fad798d7b0bb6b20
Guilherme Novak <guilherme@govakreality.com>
2024-01-22 15:40:21 -0300

    Full Screen Implementation

--- src/components/TopBar/MainMenu.vue
+++ src/components/TopBar/MainMenu.vue
@@ -1,35 +1,42 @@
 <script setup>
-  import { computed, ref } from 'vue';
-  import { useI18n } from 'vue-i18n';
-  const { t } = useI18n();
+import { computed, ref } from 'vue';
+import { useFullscreen } from '@vueuse/core'
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+const { isFullscreen, enter, exit, toggle } = useFullscreen();
 
-  const menuItems = ref([
-    { 
-      title: computed(() => t('menu.termsAndConditions')),
-      href: computed(() => t('menu.termsUrl')),
-    },
-    { title:  computed(() => t('menu.privacyPolicy')),
-      href: computed(() => t('menu.privacyUrl')),
-    }
-  ]);
+const menuItems = ref([
+  {
+    title: computed(() => t('menu.termsAndConditions')),
+    href: computed(() => t('menu.termsUrl')),
+  },
+  {
+    title: computed(() => t('menu.privacyPolicy')),
+    href: computed(() => t('menu.privacyUrl')),
+  }
+]);
+
+// const ToggleFullScreen = () => {
+
+// }
+
 </script>
 <template>
   <v-menu transition="scroll-y-transition">
     <template v-slot:activator="{ props }">
       <v-btn icon="mdi-menu" v-bind="props" size="large" color="white" class="g-bt-menu"></v-btn>
     </template>
-  
+
     <v-list class="mt-2 text-center" item-type="link">
-      <v-list-item
-      v-for="(item, i) in menuItems"
-      :key="i"
-      :value="item"
-      :href="item.href"
-      link
-      target="_blank"
-      >
+      <v-list-item ref="element">
+        <v-list-item-title class="g-menu-item" @click="toggle">{{ $t('menu.toggleFullScreen')
+        }}</v-list-item-title>
+        <!-- <v-list-item-title class="g-menu-item" @click="ToggleFullScreen">{{ $t('menu.toggleFullScreen')
+        }}</v-list-item-title> -->
+      </v-list-item>
+      <v-list-item v-for="(item, i) in menuItems" :key="i" :value="item" :href="item.href" link target="_blank">
         <v-list-item-title class="g-menu-item">{{ item.title }}</v-list-item-title>
-      </v-list-item> 
+      </v-list-item>
     </v-list>
   </v-menu>
 </template>
@@ -38,7 +45,7 @@
 <style scoped>
 .g-bt-menu {
   pointer-events: auto;
-	background-color: #cccccc42 !important;
+  background-color: #cccccc42 !important;
 }
 
 @media (min-width: 2560px) {
@@ -47,6 +54,7 @@
     height: 64px !important;
     width: 64px !important;
   }
+
   .g-menu-item {
     font-size: 30px;
     padding: 20px 40px;
